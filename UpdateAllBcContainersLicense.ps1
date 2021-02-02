e4ddb3ed606f059ac9a593a34614bbd9cb56f233
Hugi Freyr Einarsson <hugi.freyr.einarsson@advania.is>
2021-02-02 20:36:06 +0000

    UpdateAllBcContainersLicense added to master

---
+++ UpdateAllBcContainersLicense.ps1
@@ -0,0 +1,39 @@
+
+# BcContainerHelper is needed to use this function. See UpdateBcContainerHelper.ps1
+
+function Get-BCDevInstallLicense () 
+{
+    #License files to use in if else statement below
+    $BC14License = "E:\License\BC14.flf"
+    $BC17License = "E:\License\BC17.flf"
+
+    #Define name of containers that script should ignore
+    $ignoreList = "magical_cannon", "portainer"
+
+    $Containers = docker ps -a --format "{{.Names}}" --filter status=running 
+    foreach ( $tmp in $Containers) {
+        if ( $ignoreList -inotcontains $tmp) {
+            try {
+                Write-Output $tmp
+                $version = Get-BcContainerNavVersion -containerOrImageName $tmp
+                Write-Output $version
+                
+                if ($version -like "14.*") {
+                    Import-BcContainerLicense -containerName $tmp -licenseFile $BC14License  
+                }
+                elseif ($version -like "17.*") {
+                    Import-BcContainerLicense -containerName $tmp -licenseFile $BC17License  
+                }
+                else {
+                    Write-Output "Version not defined in script..."
+                }
+            }
+            catch {
+                Write-Output "Unable to get Container BC version for container $tmp"
+            }
+        }
+        else {
+            Write-Output "$tmp is on ignore list"
+        }
+    }
+}
